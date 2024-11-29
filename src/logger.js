378bc2512a24fc3df8f6fe8fd68929d4cfe81618
GusGD <si_gustavo@outlook.com>
2024-11-29 17:13:57 -0300

    Adicionado a deleção do log-times.txt para as 18:04, removido os console.log para evitar poluição.

--- src/logger.js
+++ src/logger.js
@@ -1,32 +1,35 @@
 const fs = require("fs");
 const path = require("path");
+const moment = require("moment-timezone");
 
 const logFilePath = path.join(__dirname, "../logs/log.txt");
 const timeCheckLogPath = path.join(__dirname, "../logs/log-times.txt");
 
 function writeLog(message) {
-  const logMessage = `${new Date().toISOString()} - ${message}\n`;
+  const logMessage = `${moment()
+    .tz("America/Sao_Paulo")
+    .format("YYYY-MM-DD HH:mm:ss")} - ${message}\n`;
   fs.appendFileSync(logFilePath, logMessage, "utf8");
 }
 
 function logCheckTime(checkTime) {
-  const logMessage = `${new Date().toISOString()} - Verificação de horário: ${checkTime}\n`;
+  const logMessage = `${moment()
+    .tz("America/Sao_Paulo")
+    .format("YYYY-MM-DD HH:mm:ss")} - Script Iniciado: ${checkTime}\n`;
   fs.appendFileSync(timeCheckLogPath, logMessage, "utf8");
 }
 
 function deleteLogTimesIfNeeded() {
-  const now = new Date();
+  const now = moment().tz("America/Sao_Paulo");
   if (
-    now.getHours() === 18 &&
-    now.getMinutes() === 9 &&
+    now.hour() === 18 &&
+    now.minute() === 4 &&
     fs.existsSync(timeCheckLogPath)
   ) {
     try {
       fs.unlinkSync(timeCheckLogPath);
-      console.log("Arquivo log-times.txt excluído às 19h.");
-      writeLog("Arquivo log-times.txt excluído às 19h.");
+      writeLog("Arquivo log-times.txt excluído às: " + now.format("HH:mm"));
     } catch (err) {
-      console.error("Erro ao excluir log-times.txt:", err.message);
       writeLog(`Erro ao excluir log-times.txt: ${err.message}`);
     }
   }
