9111da705b214dc0111c549ae6a7e0d55cf039fa
GusGD <si_gustavo@outlook.com>
2024-11-29 17:15:23 -0300

    adicionado mensagem de iniciado de script

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ function checkAndClockIn() {
   const now = new Date();
   const checkTime = format(now, "HH:mm:ss");
 
-  console.log(`Verificando horário: ${checkTime}`);
   logCheckTime(checkTime);
 
   clockIn(now);
 }
-
+console.log("Script de ponto iniciado ás: " + format(new Date(), "HH:mm:ss"));
 setInterval(checkAndClockIn, 60 * 1000);
