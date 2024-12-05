49ab1e3a828035d2d329eb7e966b6ecf34c129f1
GusGD <si_gustavo@outlook.com>
2024-12-04 21:54:11 -0300

    Adicionado o consumo da api de feriados

--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,36 @@
-const { format } = require("date-fns");
+import { format } from "date-fns";
+import fetch from "node-fetch";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 function isWeekend(date) {
   const day = date.getDay();
   return day === 0 || day === 6;
 }
 
-function isHoliday(date) {
-  const holidays = ["01-01", "25-12"];
-  const dateStr = format(date, "dd-MM");
-  return holidays.includes(dateStr);
-}
+async function isHoliday(date) {
+  const urlApi = process.env.URL_API;
+  const tokenApi = process.env.TOKEN_API;
+  const state = process.env.STATE;
 
-module.exports = { isWeekend, isHoliday };
+  try {
+    const year = date.getFullYear();
+
+    const response = await fetch(`${urlApi}${year}?token=${tokenApi}`);
+
+    if (!response.ok) {
+      console.error(
+        `Erro ao buscar feriados: ${response.status} - ${response.statusText}`
+      );
+      return [];
+    }
+    const data = await response.json();
+    const holidays = data.map((holiday) => holiday.date);
+    return holidays;
+  } catch (error) {
+    console.error(`Erro ao verificar feriado: ${error.message}`);
+    return [];
+  }
+}
+export { isWeekend, isHoliday };
