f4814b6c8a6a941f17bc27d6a0dc4df79a52f39e
GusGD <si_gustavo@outlook.com>
2024-12-04 21:53:30 -0300

    Refatorado

--- src/logger.js
+++ src/logger.js
@@ -1,29 +1,34 @@
-const fs = require("fs");
-const path = require("path");
-const moment = require("moment-timezone");
+import fs from "fs";
+import path from "path";
+import moment from "moment-timezone";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const logFilePath = path.join(__dirname, "../logs/log.txt");
 const timeCheckLogPath = path.join(__dirname, "../logs/log-times.txt");
 
-function writeLog(message) {
+export function writeLog(message) {
   const logMessage = `${moment()
     .tz("America/Sao_Paulo")
     .format("YYYY-MM-DD HH:mm:ss")} - ${message}\n`;
   fs.appendFileSync(logFilePath, logMessage, "utf8");
 }
 
-function logCheckTime(checkTime) {
+export function logCheckTime(checkTime) {
   const logMessage = `${moment()
     .tz("America/Sao_Paulo")
     .format("YYYY-MM-DD HH:mm:ss")} - Script Iniciado: ${checkTime}\n`;
   fs.appendFileSync(timeCheckLogPath, logMessage, "utf8");
 }
 
-function deleteLogTimesIfNeeded() {
+export function deleteLogTimesIfNeeded() {
   const now = moment().tz("America/Sao_Paulo");
   if (
     now.hour() === 18 &&
-    now.minute() === 4 &&
+    now.minute() === 10 &&
     fs.existsSync(timeCheckLogPath)
   ) {
     try {
@@ -37,5 +42,3 @@ function deleteLogTimesIfNeeded() {
 }
 
 setInterval(deleteLogTimesIfNeeded, 60000);
-
-module.exports = { writeLog, logCheckTime };
