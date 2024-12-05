2d8f12aba36c295edb77123f936d212d45784171
GusGD <si_gustavo@outlook.com>
2024-12-04 21:52:07 -0300

    Melhoria na execução do clockIn

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-const { clockIn } = require("./src/clockIn");
-const { logCheckTime } = require("./src/logger");
-const { format } = require("date-fns");
+import { clockIn } from "./src/clockIn.js";
+import { logCheckTime } from "./src/logger.js";
+import { format } from "date-fns";
 
-function checkAndClockIn() {
+let isClockingIn = false;
+
+async function checkAndClockIn() {
   const now = new Date();
   const checkTime = format(now, "HH:mm:ss");
 
   logCheckTime(checkTime);
 
-  clockIn(now);
+  if (isClockingIn) {
+    console.log(
+      "Já estamos registrando o ponto. Aguardando próxima tentativa..."
+    );
+    return;
+  }
+
+  isClockingIn = true;
+  await clockIn(now);
+  isClockingIn = false;
 }
-console.log("Script de ponto iniciado ás: " + format(new Date(), "HH:mm:ss"));
-setInterval(checkAndClockIn, 60 * 1000);
+
+console.log("Script de ponto iniciado às: " + format(new Date(), "HH:mm:ss"));
+setInterval(checkAndClockIn, 60 * 100);
